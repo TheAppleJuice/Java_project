7f7bf1a25bc69cbe70bc91ac50ce97427bbb165e
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2020-12-07 16:19:57 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/Sebastian/Bocaciu/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.Sebastian.Bocaciu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
